b1bb7e07098ffbd4f65aea2fe3b6dbe71fe49dea
toastynerd <tylermorgan86@gmail.com>
2018-12-19 18:38:32 -0800

    add ability to draw a created room

--- src/room.c
+++ src/room.c
@@ -10,6 +10,28 @@
 #include "slash.h"
 #include "room.h"
 
+// internal methods, mostly helpers for other functions
+
+int draw_end_row(int y, int leftx, int cols)
+{
+	int i;
+	for(i = 0; i < (cols + 1); i++)
+		mvaddch(y,leftx + i, '-');
+	return 0;
+}
+
+int draw_light_row(int y, int leftx, int cols)
+{
+	int i;
+	mvaddch(y, leftx, '|');
+	mvaddch(y, leftx + cols, '|');
+	for (i = 1; i < (cols); i++)
+		mvaddch(y, leftx + i, '.');
+	return 0;
+}
+
+// room methods
+
 int init_room(room *r, int itopx, int itopy, int ibotx, int iboty)
 {
 	if (ibotx <= itopx || iboty <= itopy)
@@ -32,7 +54,12 @@ int room_delta_y(room *r)
 	return r->boty - r->topy;
 }
 
-int drawroom(room *r)
+int draw_room(room *r)
 {
+	int i;
+	draw_end_row(r->topy, r->topx, room_delta_x(r));
+	draw_end_row(r->boty, r->topx, room_delta_x(r));
+	for(i = 1; i < room_delta_y(r); i++)
+		draw_light_row(r->topy + i, r->topx, room_delta_x(r));
 	return 0;
 }
